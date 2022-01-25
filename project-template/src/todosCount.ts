154ab39862e7340a9c2595d9728047a727b742b5
Avquiet <78691610+Avquiet@users.noreply.github.com>
2022-01-25 23:25:30 +0300

    Revert "Added hw-4  --25.01.2022"
    This reverts commit a86ccf5f103782abf7ce03457528e0d6561fc66a.

--- project-template/src/todosCount.ts
+++ project-template/src/todosCount.ts
@@ -1,14 +1,7 @@
-export interface responseTodo {
-  id: number,
-  userId: number,
-  title: string,
-  completed: boolean
-}
-
 export function getTodosByCount(count: number) {
   for(count = 0; count < 10; count++) {
     fetch('https://jsonplaceholder.typicode.com/users/1/todos')
-      .then<responseTodo>((response) => response.json())
+      .then<Response>((response) => response.json())
       .then((json) => console.log(json))
   }
 } 
